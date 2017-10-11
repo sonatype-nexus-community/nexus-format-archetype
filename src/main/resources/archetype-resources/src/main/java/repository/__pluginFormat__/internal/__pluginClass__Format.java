a41ecf926932e637c7751478fba0756d873c33da
Mark Dodgson <mdodgson@sonatype.com>
2017-10-11 10:49:11 +0100

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/repository/__pluginFormat__/internal/__pluginClass__Format.java
@@ -0,0 +1,33 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2017-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package ${package}.repository.${pluginFormat}.internal;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.sonatype.nexus.repository.Format;
+
+/**
+ * ${pluginClass} repository format.
+ */
+@Named(${pluginClass}Format.NAME)
+@Singleton
+public class ${pluginClass}Format
+    extends Format
+{
+  public static final String NAME = "${pluginFormat}";
+
+  public ${pluginClass}Format() {
+    super(NAME);
+  }
+}
---
+++ src/main/resources/archetype-resources/src/main/java/repository/__pluginFormat__/internal/__pluginClass__FormatSecurityContributor.java
@@ -0,0 +1,34 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2017-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package ${package}.repository.${pluginFormat}.internal;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.sonatype.nexus.repository.Format;
+import org.sonatype.nexus.repository.security.RepositoryFormatSecurityContributor;
+
+/**
+ * ${pluginClass} format security resource.
+ */
+@Named
+@Singleton
+public class ${pluginClass}FormatSecurityContributor
+    extends RepositoryFormatSecurityContributor
+{
+  @Inject
+  public ${pluginClass}FormatSecurityContributor(@Named(${pluginClass}Format.NAME) final Format format) {
+    super(format);
+  }
+}
