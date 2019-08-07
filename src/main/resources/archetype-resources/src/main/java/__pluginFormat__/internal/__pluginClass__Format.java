5f7a80482e715f7b2d1536b4170e06fef01f2b9c
Dan Rollo <danrollo@gmail.com>
2019-08-07 15:45:43 -0400

    fix IDE mangle

--- src/main/resources/archetype-resources/src/main/java/__pluginFormat__/internal/__pluginClass__Format.java
+++ src/main/resources/archetype-resources/src/main/java/__pluginFormat__/internal/__pluginClass__Format.java
@@ -10,10 +10,12 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-package $ import javax.inject.Named;
+package ${package}.${pluginFormat}.internal;
+
+import javax.inject.Named;
 import javax.inject.Singleton;
 
-{package}.${pluginFormat}.internal;
+import org.sonatype.nexus.repository.Format;
 
 /**
  * ${pluginClass} repository format.
