e7d3cf0525db8dc9d10b052012bdb35e721c7726
Dan Rollo <danrollo@gmail.com>
2019-08-08 11:56:42 -0400

    use separate 'year' to update IT reference source files (to fix IT's)

--- src/test/resources/projects/it1/reference/src/main/resources/static/rapture/NX/foo/util/FooRepositoryUrls.js
+++ src/test/resources/projects/it1/reference/src/main/resources/static/rapture/NX/foo/util/FooRepositoryUrls.js
@@ -1,6 +1,6 @@
 /*
  * Sonatype Nexus (TM) Open Source Version
- * Copyright (c) ${currentYear}-present Sonatype, Inc.
+ * Copyright (c) ${ITReferenceYear}-present Sonatype, Inc.
  * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
  *
  * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
